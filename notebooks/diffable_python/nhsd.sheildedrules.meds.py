c740d74a7a32b2aebb5cb9cf6c6ae0d87d87d2fb
Brian <brianmkcebm@gmail.com>
2020-03-29 17:15:43 +0100

    update markdown on nhsd repo

--- notebooks/diffable_python/nhsd.sheildedrules.meds.py
+++ notebooks/diffable_python/nhsd.sheildedrules.meds.py
@@ -32,7 +32,7 @@ import pandas as pd
 sql = '''
 WITH bnf_codes AS (  
   SELECT bnf_code FROM hscic.presentation WHERE 
-  ##transplant
+  (##transplant
   bnf_code LIKE '0802%' OR # the following meds are listed in definition but annex looks like they included all meds in this section
   ##respiratory
   bnf_code LIKE '030302%' OR #BNF leukotriene antagonists
@@ -40,7 +40,9 @@ WITH bnf_codes AS (
   bnf_code LIKE '030101%' OR #BNF adrenoceptor aganosts
   bnf_code LIKE '0302%' OR #BNF corticosteroids resp
   bnf_code LIKE '0303030B0%' OR #BNF roflumilast 
-  bnf_code LIKE '030102%' #BNF antimuscarinin brochodilators
+  bnf_code LIKE '030102%') #BNF antimuscarinin brochodilators
+  AND
+  bnf_code NOT LIKE '0802020T0%XAX' #BNF tacrolimus mouthwash
    )
 SELECT "vmp" AS type, id, bnf_code, nm
 FROM dmd.vmp
@@ -56,7 +58,24 @@ ORDER BY bnf_code, type, id'''
 
 nhsd_shieldedrules_meds = bq.cached_read(sql, csv_path=os.path.join('..','data','nhsd_shieldedrules_meds .csv'))
 pd.set_option('display.max_rows', None)
-nhsd_shieldedrules_meds
+nhsd_shieldedrules_meds.head(10)
 # -
 
+# The [CMO guidance on "at risk"](https://www.gov.uk/government/publications/guidance-on-shielding-and-protecting-extremely-vulnerable-persons-from-covid-19/guidance-on-shielding-and-protecting-extremely-vulnerable-persons-from-covid-19) is defined as
+#
+# 1. Solid organ transplant recipients.
+# 2. People with specific cancers:
+#   - people with cancer who are undergoing active chemotherapy or radical radiotherapy for lung cancer
+#   - people with cancers of the blood or bone marrow such as leukaemia, lymphoma or myeloma who are at any stage of treatment
+#   - people having immunotherapy or other continuing antibody treatments for cancer
+#   - people having other targeted cancer treatments which can affect the immune system, such as protein kinase inhibitors or PARP inhibitors
+#   - people who have had bone marrow or stem cell transplants in the last 6 months, or who are still taking immunosuppression drugs
+#   - People with severe respiratory conditions including all cystic fibrosis, severe asthma and severe COPD.
+# 3. People with rare diseases and inborn errors of metabolism that significantly increase the risk of infections (such as SCID, homozygous sickle cell).
+# 4. People on immunosuppression therapies sufficient to significantly increase risk of infection.
+# 5. Women who are pregnant with significant heart disease, congenital or acquired.
 
+# The NHS Digital medicines supports identification of transplant recipients (it might not identify all these patients as in some places osp supplies direct), sever asthma/COPD but medicines could be used on all other points with additional clinical codes. Some ideas
+# - Pregancy related code plus prescription of medicines related to heart disease ([see GitHub repo](https://github.com/ebmdatalab/cvd-covid-codelist-notebook))
+# - Cystic Fibrosis code or [prescription of colistin](https://openprescribing.net/analyse/#org=CCG&numIds=0501070I0&denom=nothing&selectedTab=summary) (in some areas this is supplied via shared care)
+# - immnuosuppression plus increased risk of infection ([see BSRM guidance](https://www.rheumatology.org.uk/Portals/0/Documents/Rheumatology_advice_coronavirus_immunosuppressed_patients_220320.pdf?ver=2020-03-23-165636-767))
